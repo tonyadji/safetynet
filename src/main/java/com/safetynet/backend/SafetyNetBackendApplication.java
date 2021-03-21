38dfe6d8471856229b25433b1288f8a328a1ffac
N9-T <tonysimonadji@gmail.com>
2021-03-21 18:13:48 +0100

    initial commit

---
+++ src/main/java/com/safetynet/backend/SafetyNetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/backend/SafetyNetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
