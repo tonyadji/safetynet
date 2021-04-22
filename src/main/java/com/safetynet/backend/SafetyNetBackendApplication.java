106cd39a1f023bc0ef1108da749ac1f16f1d6be6
tonys <tonys@DESKTOP-UCSN0OM.ufipay.local>
2021-04-22 17:28:07 +0100

    improve coverage

--- src/main/java/com/safetynet/backend/SafetyNetBackendApplication.java
+++ src/main/java/com/safetynet/backend/SafetyNetBackendApplication.java
@@ -3,23 +3,11 @@ package com.safetynet.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.safetynet.backend.store.DataStore;
-import com.safetynet.backend.store.IManageDataStore;
-import com.safetynet.backend.store.ManageWithJsonFIle;
-
 @SpringBootApplication
 public class SafetyNetBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetBackendApplication.class, args);
-		
-		IManageDataStore storeManager = new ManageWithJsonFIle();
-		try {
-			DataStore ds = storeManager.getDataStoreInstance();
-			System.out.println("store initialized with "+ds.getPersons().size()+" persons");
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
